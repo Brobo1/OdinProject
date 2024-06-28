2132dcb0e959b93d318e475e782d9acff3fa2d07
Brobo1 <armanahz95@gmail.com>
2024-06-28 16:39:10 +0200

    ``` Refactor calculator layout and add symbol buttons
    Updated CSS to reorganize calculator layout with separate number and symbol sections. Integrated new "Roboto" font for consistent styling. Enhanced script.js to dynamically create number and symbol buttons for better maintainability.
    ```

--- projects/calculator-project/script.js
+++ projects/calculator-project/script.js
@@ -1,13 +1,16 @@
 const numbers = document.getElementById("numbers");
+const symbols = document.getElementById("symbols");
 let num1;
 let num2;
 let operator;
 
 const addNumbers = () => {
-  let numberPad = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, "", "="];
+  // let numberPad = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0", "", "="];
+  let numberPad = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0", "="];
   numberPad.forEach((number) => {
     let numBtn = document.createElement("button");
-    numBtn.innerText = number.toString();
+    numBtn.type = "button";
+    numBtn.innerText = number;
     if (numBtn.innerText === "") {
       numBtn.disabled = true;
     }
@@ -16,7 +19,22 @@ const addNumbers = () => {
   });
 };
 
-addNumbers();
+const addSymbols = () => {
+  let symbolPad = ["/", "*", "-", "+"];
+  symbolPad.forEach((symbol) => {
+    let symBtn = document.createElement("button");
+    symBtn.type = "button";
+    symBtn.innerText = symbol;
+    symBtn.className = "symbol";
+    symbols.appendChild(symBtn);
+  });
+};
+
+const createPad = () => {
+  addNumbers();
+  addSymbols();
+};
+createPad();
 
 function add(a, b) {
   return a + b;
