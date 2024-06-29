108a7beb20b84c0ea16660537f60219030ff3bdb
Brobo1 <armanahz95@gmail.com>
2024-06-29 22:51:29 +0200

    Add clear and delete buttons to calculator
    Added clear and delete buttons to the calculator's HTML. Updated CSS file for better button styling and layout management. Made changes to JavaScript to handle the added functionality of the clear and delete buttons, and to adjust number data type conversion from integer to float. Also, extended number pad with the addition of a decimal point button.

--- projects/calculator-project/script.js
+++ projects/calculator-project/script.js
@@ -1,13 +1,15 @@
 const numbers = document.getElementById("numbers");
 const symbols = document.getElementById("symbols");
-const numberPad = document.getElementById("number-pad");
+const buttons = document.getElementById("buttons");
 const displayScr = document.getElementById("screenText");
+const deleteBtn = document.getElementById("delete");
+const clearBtn = document.getElementById("clear");
 let num1;
 let num2;
 let operator;
 
 const addNumbers = () => {
-  let numberPad = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0", "C", "="];
+  let numberPad = ["7", "8", "9", "4", "5", "6", "1", "2", "3", ".", "0", "="];
   numberPad.forEach((number) => {
     let numBtn = document.createElement("button");
     numBtn.type = "button";
@@ -43,6 +45,7 @@ const createPad = () => {
   addNumbers();
   addSymbols();
 };
+
 createPad();
 
 function add(a, b) {
@@ -62,8 +65,8 @@ function divide(a, b) {
 }
 
 function operate(num1, operator, num2) {
-  num1 = parseInt(num1);
-  num2 = parseInt(num2);
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
   if (operator === "+") return add(num1, num2);
   if (operator === "-") return subtract(num1, num2);
   if (operator === "*") return multiply(num1, num2);
@@ -75,15 +78,16 @@ function populateDisplay(str) {
 }
 
 let text = "";
-numberPad.addEventListener("click", (e) => {
+buttons.addEventListener("click", (e) => {
   let name = e.target.name;
+  console.log(name);
   if (name === "number" || name === "symbol") {
     text += e.target.innerText;
   }
   if (name === "equals") {
     let vars = text.match(/\d+|\S/g);
     text = operate(...vars);
-    console.log(operate(...vars));
+    console.log(vars);
   }
   if (name === "clear") {
     text = "";
