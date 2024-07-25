196d98f16d777987b4c520e3467061f46c47abcb
Brobo1 <armanahz95@gmail.com>
2024-07-26 00:51:40 +0200

    Refactor GameBoard and add Cell functionality
    - Removed `player` parameter from `GameBoard` function.
    - Modified gameBoard cells to use the new `Cell` function.
    - Added `printGameBoard` method to GameBoard for console output.
    - Added `Cell` function to manage cell state with `addSign` and `getSign` methods.

--- full-stack-js/javascript/projects/tic-tac-toe/script.js
+++ full-stack-js/javascript/projects/tic-tac-toe/script.js
@@ -1,18 +1,20 @@
-function GameBoard(player) {
+function GameBoard() {
   const row = 3;
   const col = 3;
   const gameBoard = [];
   for (let i = 0; i < row; i++) {
     gameBoard[i] = [];
     for (let j = 0; j < col; j++) {
-      gameBoard[i].push(j);
+      gameBoard[i].push(Cell());
     }
   }
 
-  const setGameBoard = () => {};
+  const setGameBoard = (player) => {};
 
   const getGameBoard = () => gameBoard;
-  return { getGameBoard };
+  const printGameBoard = () =>
+    gameBoard.map((row) => console.log(row.map((cell) => cell.getSign())));
+  return { getGameBoard, printGameBoard };
 }
 
 function Player(name, sign) {
@@ -31,7 +33,15 @@ function GameFlow() {
     p2: new Player("player2", "o"),
   };
   let gameBoard = new GameBoard();
-  console.log(gameBoard.getGameBoard());
+  gameBoard.printGameBoard();
+}
+
+function Cell() {
+  let sign = " ";
+
+  const addSign = (player) => (sign = player.sign);
+  const getSign = () => sign;
+  return { addSign, getSign };
 }
 
 GameFlow();
