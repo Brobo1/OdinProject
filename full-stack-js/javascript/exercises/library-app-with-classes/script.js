e9890a158bf3cee27788fc019a964800362c335f
Brobo1 <armanahz95@gmail.com>
2024-08-27 12:32:21 +0200

    Refactor Book function into ES6 class
    - Replaced Book constructor function with ES6 class syntax
    - Preserved existing functionality and methods within the new class structure

--- full-stack-js/javascript/exercises/library-app-with-classes/script.js
+++ full-stack-js/javascript/exercises/library-app-with-classes/script.js
@@ -7,14 +7,16 @@ const myLibrary = [];
 let isModal = false;
 let formData;
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"}`;
-  };
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+      return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"}`;
+    };
+  }
 }
 
 function addBookToLibrary(book) {
