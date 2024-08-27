86c7b9ab781874ebde036b857a1642122f40a1d9
Brobo1 <armanahz95@gmail.com>
2024-08-27 14:54:28 +0200

    Add paragraph element to index.js
    - Create and append a paragraph element with text "live test" to the DOM
    - Ensure the new element is added below the image element

--- full-stack-js/javascript/exercises/webpack-exercise/src/index.js
+++ full-stack-js/javascript/exercises/webpack-exercise/src/index.js
@@ -5,6 +5,10 @@ import odinImage from "./odin.png";
 const image = document.createElement("img");
 image.src = odinImage;
 
+const str = document.createElement("p");
+str.innerText = "live test";
+
 document.body.appendChild(image);
+document.body.appendChild(str);
 
 console.log(greeting);
