6ac7919ff2f92739202637029cbd5c0ff5e8c456
Brobo1 <armanahz95@gmail.com>
2024-08-27 14:48:57 +0200

    Add image support and webpack-dev-server to Webpack exercise
    - Add odin.png image to the project.
    - Update package.json to include webpack-dev-server.
    - Modify webpack.config.js to handle image assets.
    - Update index.js to display the odin.png image.
    - Update package-lock.json with new dependencies.

--- full-stack-js/javascript/exercises/webpack-exercise/src/index.js
+++ full-stack-js/javascript/exercises/webpack-exercise/src/index.js
@@ -1,4 +1,10 @@
 import { greeting } from "./greeting.js";
 import "./styles.css";
+import odinImage from "./odin.png";
+
+const image = document.createElement("img");
+image.src = odinImage;
+
+document.body.appendChild(image);
 
 console.log(greeting);
