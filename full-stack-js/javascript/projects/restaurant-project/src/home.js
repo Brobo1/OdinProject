901a79d8285aaffe2300d9dbfee4305041de9227
Brobo1 <armanahz95@gmail.com>
2024-08-30 23:30:26 +0200

    Add restaurant image to home page
    - Imported and added image to the home page.
    - Updated CSS to style the new image.

--- full-stack-js/javascript/projects/restaurant-project/src/home.js
+++ full-stack-js/javascript/projects/restaurant-project/src/home.js
@@ -1,3 +1,5 @@
+import image from "./restYou.png";
+
 export function homeContent(container) {
   const content = document.createElement("h1");
   content.textContent = "home";
@@ -11,6 +13,7 @@ function homePage() {
       Welcome to wtf aka Where's the Food?!
     </p>
     <p class="text">We have no chef so you'll have to make your own food!</p>
+    <img class="rest-img" src="${image}" alt="You making food">
     <p class="text">We have the freshest ingredients leftover from previous customers or provided by yourself</p>
   </div>
   `;
