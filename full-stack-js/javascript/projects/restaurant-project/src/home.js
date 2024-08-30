fc70355c52ce862bc45dee2fb07113e177306b96
Brobo1 <armanahz95@gmail.com>
2024-08-30 20:31:54 +0200

    Update home page content and styles
    - Refactor `homeContent` to use `homePage` template
    - Add new welcome message and descriptions to home page
    - Adjust padding and margins in `.flex-container`
    - Update styles for home page content, including text alignment, font size, and colors

--- full-stack-js/javascript/projects/restaurant-project/src/home.js
+++ full-stack-js/javascript/projects/restaurant-project/src/home.js
@@ -1,5 +1,19 @@
 export function homeContent(container) {
   const content = document.createElement("h1");
   content.textContent = "home";
-  container.appendChild(content);
+  container.innerHTML = homePage();
+}
+
+function homePage() {
+  const str = `
+  <div>
+    <p class="home-title">
+      Welcome to wtf aka Where's the Food?!
+    </p>
+    <p class="text">We have no chef so you'll have to make your own food!</p>
+    <p class="text">We have the freshest ingredients leftover from previous customers or provided by yourself</p>
+  </div>
+  `;
+
+  return str;
 }
