bdc9d0f01be9de20d02309cfb64c6d919f692476
Brobo1 <armanahz95@gmail.com>
2024-08-30 20:43:20 +0200

    Refactor `about.js` to use `innerHTML`
    - Replaced `appendChild` with `innerHTML` for markup insertion.
    - Added `aboutPage` function to return HTML string.

--- full-stack-js/javascript/projects/restaurant-project/src/about.js
+++ full-stack-js/javascript/projects/restaurant-project/src/about.js
@@ -1,5 +1,20 @@
 export function aboutContent(container) {
   const content = document.createElement("h1");
   content.textContent = "about";
-  container.appendChild(content);
+  container.innerHTML = aboutPage();
+}
+
+function aboutPage() {
+  const str = `
+  <div>
+    <p class="text">
+      hmmmmmmmmmm
+    </p>
+    <p class="text">
+      It works
+    </p>
+  </div>
+  `;
+
+  return str;
 }
