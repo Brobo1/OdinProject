8111c91e0db12576d02d4ed823a62ad3c2b3e4df
Brobo1 <armanahz95@gmail.com>
2024-08-30 20:43:05 +0200

    Refactor menuContent function to include detailed menu page layout
    - Replaced existing text content with a structured HTML template
    - Introduced a new `menuPage` function to generate the HTML string
    - Updated `menuContent` to use `innerHTML` instead of `appendChild`

--- full-stack-js/javascript/projects/restaurant-project/src/menu.js
+++ full-stack-js/javascript/projects/restaurant-project/src/menu.js
@@ -1,5 +1,21 @@
 export function menuContent(container) {
   const content = document.createElement("h1");
   content.textContent = "menu";
-  container.appendChild(content);
+  container.innerHTML = menuPage();
+}
+
+function menuPage() {
+  const str = `
+  <div>
+
+    <p class="text">
+      Our menu is your menu.
+    </p>
+    <p class="text">
+      We don't have one, so you have to bring your own recipes.
+    </p>
+  </div>
+  `;
+
+  return str;
 }
