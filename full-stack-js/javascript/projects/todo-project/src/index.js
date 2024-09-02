6429a9ff786e8131478f35f56e2595c7431d15d3
Brobo1 <armanahz95@gmail.com>
2024-09-02 23:45:44 +0200

    Add basic structure and styling for Todo app
    - Added Google Fonts integration for Roboto in template.html.
    - Replaced placeholder paragraph with a container div in template.html.
    - Created initial CSS styles in styles.css.
    - Implemented basic JS functionality to dynamically generate and display a Todo card in index.js.

---
+++ full-stack-js/javascript/projects/todo-project/src/index.js
@@ -0,0 +1,42 @@
+import "./styles.css";
+
+const container = document.getElementById("container");
+
+function createTodo(
+  title,
+  description,
+  dueDate,
+  priority,
+  notes = [],
+  checklist = [],
+) {
+  return { title, description, dueDate, priority, notes, checklist };
+}
+
+const testTodo = createTodo(
+  "ayoo",
+  "this is ayo...",
+  "tomorrow",
+  9,
+  ["One", "Two", "Three", "Four", "Five", "six."],
+  ["One", "Two", "Three", "Four", "Five", "six?"],
+);
+
+function todoCard(todo) {
+  const notes = todo.notes.map((note) => `<p>${note}</p>`).join(" ");
+  const checklist = todo.checklist.map((item) => `<p>${item}</p>`).join(" ");
+
+  const card = `
+  <div class="card-container">
+    <p class="card-title">${todo.title}</p>
+    <p class="card-description">${todo.description}</p>
+    <p class="card-duedate">${todo.dueDate}</p>
+    <p class="card-priority">${todo.priority}</p>
+    <div class="card-notes">${notes}</div>
+    <div class="card-checklist">${checklist}</div>
+  </div>
+  `;
+  return card;
+}
+
+container.innerHTML = todoCard(testTodo);
