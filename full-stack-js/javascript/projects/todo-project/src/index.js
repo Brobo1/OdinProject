f96184ee1be17fb25f10675d5604c44a6105661c
Brobo1 <armanahz95@gmail.com>
2024-09-12 21:48:43 +0200

    Add edit icon functionality to todo card items
    - Implemented `showEdit` function to display edit icon on hover
    - Enabled content editing for title, description, due date, and priority
    - Updated CSS styles for card-item-container and card-edit-icon
    - Added new edit icon asset (pencil-svgrepo-com.svg)

--- full-stack-js/javascript/projects/todo-project/src/index.js
+++ full-stack-js/javascript/projects/todo-project/src/index.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import arrowDown from "./assets/arrowDown.svg";
+import editPen from "./assets/pencil-svgrepo-com.svg";
 import * as test from "./components/test-data.js";
 
 const container = document.getElementById("container");
@@ -17,13 +18,18 @@ function todoCard(todo) {
     .join(" ");
   return `
   <div class="card-container">
-    <div class="card-title-container">
+    <div class="card-item-container card-title-container">
       <p class="card-component card-title">${todo.title}</p>
-      <img class="arrow-icon" src="${arrowDown}" alt="arrow icon">
     </div>
-    <p class="card-component card-description">${todo.description}</p>
-    <p class="card-component card-duedate">${todo.dueDate}</p>
-    <p class="card-component card-priority">${todo.priority}</p>
+    <div class="card-item-container card-description-container">
+      <p class="card-component card-description">${todo.description}</p>
+    </div>
+    <div class="card-item-container card-duedate-container">
+      <p class="card-component card-duedate">${todo.dueDate}</p>
+    </div>
+    <div class="card-item-container card-priority-container">
+      <p class="card-component card-priority">${todo.priority}</p>
+    </div>
     <div class="card-component card-notes">${notes}</div>
     <div class="card-component card-checklist">${checklist}</div>
   </div>
@@ -31,3 +37,54 @@ function todoCard(todo) {
 }
 
 container.innerHTML = todoCard(test.testTodo);
+
+function showEdit(cardContainer) {
+  let isHover = false;
+
+  container.addEventListener(
+    "mouseenter",
+    (e) => {
+      const target = e.target;
+      if (
+        target.classList.contains(`card-${cardContainer}-container`) &&
+        !isHover
+      ) {
+        isHover = true;
+        target.innerHTML += `<img src="${editPen}" class="card-edit-icon" alt="Edit Icon">`;
+
+        target
+          .querySelector(".card-edit-icon")
+          .addEventListener("click", (clickEvent) => {
+            target.children[0].contentEditable = true;
+          });
+      }
+    },
+    true,
+  );
+
+  container.addEventListener(
+    "mouseleave",
+    (e) => {
+      const target = e.target;
+      if (
+        target.classList.contains(`card-${cardContainer}-container`) &&
+        isHover
+      ) {
+        isHover = false;
+        const getChild = Array.from(target.children).filter((child) =>
+          child.classList.contains("card-edit-icon"),
+        );
+
+        if (getChild.length > 0) {
+          target.removeChild(getChild[0]);
+        }
+      }
+    },
+    true,
+  );
+}
+
+showEdit("title");
+showEdit("description");
+showEdit("duedate");
+showEdit("priority");
