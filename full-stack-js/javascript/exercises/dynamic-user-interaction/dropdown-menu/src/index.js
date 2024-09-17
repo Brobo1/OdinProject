6ae8079d04690919241237f97707fbbcae2819d0
Brobo1 <armanahz95@gmail.com>
2024-09-17 15:59:38 +0200

    Add interactive dropdown menu
    - Updated HTML structure with dropdown and items.
    - Implemented JS to handle dropdown interactions.
    - Added CSS styles for dropdown appearance.

--- full-stack-js/javascript/exercises/dynamic-user-interaction/dropdown-menu/src/index.js
+++ full-stack-js/javascript/exercises/dynamic-user-interaction/dropdown-menu/src/index.js
@@ -1,1 +1,25 @@
 import "./styles.css";
+
+function dropdownMenu() {
+  const dropdown = document.getElementById("dropdown");
+  const dropdownChildren = dropdown.children[1];
+  const dropdownItems = Array.from(dropdownChildren.children);
+
+  dropdownChildren.style.display = "none";
+
+  dropdown.addEventListener("mouseover", () => {
+    dropdownChildren.style.display = "flex";
+  });
+  dropdown.addEventListener("mouseleave", () => {
+    dropdownChildren.style.display = "none";
+  });
+
+  console.log(dropdownItems);
+  dropdownItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      console.log("clicked", item.textContent);
+    });
+  });
+}
+
+dropdownMenu();
