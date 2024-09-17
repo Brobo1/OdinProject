8b7f0c286e58abd92926183851a12d39660e0d35
Brobo1 <armanahz95@gmail.com>
2024-09-17 18:22:00 +0200

    Add auto-slide feature and improve dot navigation in image carousel
    - Implement auto-slide with reset functionality
    - Update image on arrow and dot clicks
    - Add dataset attributes to dots for navigation
    - Refactor to improve code readability and maintainability

--- full-stack-js/javascript/exercises/dynamic-user-interaction/image-carousel/src/index.js
+++ full-stack-js/javascript/exercises/dynamic-user-interaction/image-carousel/src/index.js
@@ -8,21 +8,48 @@ function imageCarousel() {
   const imageContainer = document.getElementById("image");
   const images = [image1, image2, image3, image4];
   const arrows = document.getElementById("arrows");
+  const dots = document.getElementById("dots");
+  let autoSlideInterval;
   let counter = 0;
-  imageContainer.innerHTML = `<img src="${images[counter]}" alt="${images[counter]}"/>`;
-  highlightDots(counter);
+
+  function updateImage() {
+    imageContainer.innerHTML = `<img src="${images[counter]}" alt="${images[counter]}"/>`;
+    highlightDots(counter);
+    resetAutoSlide();
+  }
+
+  function startAutoSlide() {
+    autoSlideInterval = setInterval(() => {
+      counter = (counter + 1) % images.length;
+      updateImage();
+    }, 5000);
+  }
+
+  function resetAutoSlide() {
+    clearInterval(autoSlideInterval);
+    startAutoSlide();
+  }
 
   arrows.addEventListener("click", (e) => {
     const target = e.target.className;
     if (target === "forward") {
-      counter = (counter + 1) % 4;
+      counter = (counter + 1) % images.length;
     }
     if (target === "back") {
-      counter = counter === 0 ? 3 : (counter - 1) % 4;
+      counter = counter === 0 ? 3 : (counter - 1) % images.length;
     }
-    imageContainer.innerHTML = `<img src="${images[counter]}" alt="${images[counter]}"/>`;
-    highlightDots(counter);
+    updateImage();
   });
+
+  dots.addEventListener("click", (e) => {
+    const target = e.target.dataset["dot"];
+    if (target) {
+      counter = target;
+      updateImage();
+    }
+  });
+
+  updateImage();
 }
 
 function highlightDots(dot) {
