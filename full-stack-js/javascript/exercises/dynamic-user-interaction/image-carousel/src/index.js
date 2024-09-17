1b301b39027522813ddb00c82002f425f41c27e5
Brobo1 <armanahz95@gmail.com>
2024-09-17 17:23:31 +0200

    Add image carousel feature
    - Added image assets (img1.png, img2.png, img3.png, img4.png)
    - Implemented image carousel in `index.js`
    - Updated `template.html` for carousel structure
    - Styled carousel in `styles.css`

--- full-stack-js/javascript/exercises/dynamic-user-interaction/image-carousel/src/index.js
+++ full-stack-js/javascript/exercises/dynamic-user-interaction/image-carousel/src/index.js
@@ -1,1 +1,25 @@
 import "./styles.css";
+import image1 from "./assets/img1.png";
+import image2 from "./assets/img2.png";
+import image3 from "./assets/img3.png";
+import image4 from "./assets/img4.png";
+
+function imageCarousel() {
+  const imageContainer = document.getElementById("image");
+  const images = [image1, image2, image3, image4];
+  const arrows = document.getElementById("arrows");
+  let counter = 0;
+  imageContainer.innerHTML = `<img src="${images[counter]}" alt="${images[counter]}"/>`;
+  arrows.addEventListener("click", (e) => {
+    const target = e.target.className;
+    if (target === "forward") {
+      counter = (counter + 1) % 4;
+    }
+    if (target === "back") {
+      counter = counter === 0 ? 3 : (counter - 1) % 4;
+    }
+    imageContainer.innerHTML = `<img src="${images[counter]}" alt="${images[counter]}"/>`;
+  });
+}
+
+imageCarousel();
