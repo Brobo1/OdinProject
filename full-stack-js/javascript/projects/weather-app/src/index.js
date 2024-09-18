f073b04a4d37d489d46a0f45f099ec5809d0498d
Brobo1 <armanahz95@gmail.com>
2024-09-18 18:25:48 +0200

    Update Weather App UI and Fetch Weather Data
    - Added Roboto font to styles.css and updated general styling
    - Implemented search functionality in index.js
    - Integrated weather API to fetch and display weather data
    - Modified template.html to include search input and button, weather data display area

--- full-stack-js/javascript/projects/weather-app/src/index.js
+++ full-stack-js/javascript/projects/weather-app/src/index.js
@@ -1,1 +1,49 @@
 import "./styles.css";
+
+const searchBtn = document.getElementById("search-button");
+const searchBar = document.getElementById("search-input");
+const wTable = document.getElementById("weather-table");
+const dataContainer = document.getElementById("weather-data");
+const search = document.getElementById("search-container");
+
+async function getWeather(location = "Oslo") {
+  try {
+    const res = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=WW9CBPZS3B3FM4RVE6X42RVV9 `,
+      { mode: "cors" },
+    );
+    return await res.json();
+  } catch {
+    console.log("Bad request");
+  }
+}
+
+searchBtn.addEventListener("click", async () => {
+  let weatherData;
+  weatherData = await getWeather();
+  if (searchBar.value) {
+    weatherData = await getWeather(searchBar.value);
+  }
+  weatherToday(weatherData);
+});
+
+function weatherToday(data) {
+  const location = document.createElement("div");
+  location.innerHTML = `
+    <p id="location-name">
+    ${data.resolvedAddress}
+    </p>
+  `;
+  dataContainer.prepend(location);
+
+  wTable.innerHTML = `
+    <tr>
+      <th>${data.days[0].datetime}</th>
+    </tr>
+    <tr>
+    
+    </tr>
+  `;
+}
+
+function addRow() {}
