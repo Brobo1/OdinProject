7f7efa7c1c5efa5261b573604531bbf35f9d8a53
Brobo1 <armanahz95@gmail.com>
2024-09-18 16:14:06 +0200

    Refactor fetch usage to async/await in giphy-cats script
    - Replaced fetch and promise chaining with async/await
    - Commented out the old fetch implementation
    - Consolidated response handling and image sourcing within an async function

--- full-stack-js/javascript/exercises/async-and-apis/giphy-cats/src/index.js
+++ full-stack-js/javascript/exercises/async-and-apis/giphy-cats/src/index.js
@@ -3,13 +3,22 @@ import "./styles.css";
 const image = document.createElement("img");
 document.getElementById("container").append(image);
 
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=XqjjoeJDPn8sZzHu9SLMBe2zkQEkDUlQ&s=cats",
-  { mode: "cors" },
-)
-  .then(function (res) {
-    return res.json();
-  })
-  .then(function (res) {
-    image.src = res.data.images.original.url;
-  });
+// fetch('https://api.giphy.com/v1/gifs/translate?api_key=YOUR_KEY_HERE&s=cats', {mode: 'cors'})
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(response) {
+//     img.src = response.data.images.original.url;
+//   });
+
+async function getCats() {
+  const response = await fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=XqjjoeJDPn8sZzHu9SLMBe2zkQEkDUlQ&s=cats",
+    { mode: "cors" },
+  );
+
+  const catData = await response.json();
+  image.src = catData.data.images.original.url;
+}
+
+getCats();
