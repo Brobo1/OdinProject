071e8a57c874db07716a8730910ecff994b569af
Brobo1 <armanahz95@gmail.com>
2024-09-30 09:58:33 +0200

    Refactor weather app features
    - Added CSS styling for rows
    - Introduced conversion utility (fToC function)
    - Enhanced addRow function to handle "Today" and "Tomorrow" labels

--- full-stack-js/javascript/projects/weather-app/src/components/rows.js
+++ full-stack-js/javascript/projects/weather-app/src/components/rows.js
@@ -1,12 +1,23 @@
 export function addRow(data) {
   const wRow = document.getElementById("weather-row");
+  let today;
 
-  data.forEach((day) => {
+  data.forEach((day, index) => {
     const row = document.createElement("div");
-    row.id = "row";
+    row.className = "row";
+
+    if (index === 0) {
+      today = "Today";
+    } else if (index === 1) {
+      today = "Tomorrow";
+    } else {
+      today = day.datetime;
+    }
+
     row.innerHTML = `
-			<p>${day.datetime}</p>
+			<p>${today}</p>
 		`;
+
     wRow.append(row);
   });
 }
