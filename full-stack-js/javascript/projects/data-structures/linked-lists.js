0d82c6129896e3b816879ec9629087c322a12d2c
Brobo1 <armanahz95@gmail.com>
2024-10-03 23:58:52 +0200

    Add functionality to LinkedList
    - Initialize head and tail in LinkedList constructor
    - Implement append method to add new nodes
    - Add toString method for outputting the list
    - Enhance Node constructor with value property

--- full-stack-js/javascript/projects/data-structures/linked-lists.js
+++ full-stack-js/javascript/projects/data-structures/linked-lists.js
@@ -1,3 +1,30 @@
-function LinkedList() {}
+function LinkedList() {
+  this.head = null;
+  this.append = function (value) {
+    const node = new Node(value);
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
+    this.tail = node;
+  };
 
-function Node(value = null, nextNode = null) {}
+  this.toString = () => {
+    console.log(this.head);
+  };
+}
+
+function Node(value = null, nextNode = null) {
+  this.value = value;
+}
+
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+list.toString();
