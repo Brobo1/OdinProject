1d452e31a1d2d279424af381de07d72422f10e2a
Brobo1 <armanahz95@gmail.com>
2024-10-04 15:38:22 +0200

    Add multiple methods to LinkedList class
    - Added prepend method to insert at head
    - Implemented size method to count nodes
    - Added first and last methods to get head and tail
    - Implemented at method to get value at index
    - Added pop method to remove last element
    - Revised contains method to iteratively check for value
    - Added find method to get index of value
    - Enhanced toString method for string representation of list

--- full-stack-js/javascript/projects/data-structures/linked-lists.js
+++ full-stack-js/javascript/projects/data-structures/linked-lists.js
@@ -1,5 +1,6 @@
 function LinkedList() {
   this.head = null;
+
   this.append = function (value) {
     const node = new Node(value);
     if (this.head === null) {
@@ -10,21 +11,132 @@ function LinkedList() {
     this.tail = node;
   };
 
-  this.toString = () => {
-    console.log(this.head);
+  this.prepend = (value) => {
+    const node = new Node(value);
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
   };
+
+  this.size = () => {
+    const countNodes = (node) => {
+      if (node === null) {
+        return 0;
+      }
+      return 1 + countNodes(node.next);
+    };
+    return countNodes(this.head);
+  };
+
+  this.first = () => {
+    return this.head;
+  };
+
+  this.last = () => {
+    return this.tail;
+  };
+
+  this.at = (index) => {
+    let count = 0;
+    if (index > this.size() - 1) {
+      return "Index out of bounds";
+    }
+    const returnIndex = (node) => {
+      if (index === count) {
+        return node.value;
+      }
+      count++;
+      return returnIndex(node.next);
+    };
+    return returnIndex(this.head);
+  };
+
+  this.pop = () => {
+    if (this.head === null) return null;
+
+    if (this.head === this.tail) {
+      const value = this.head.value;
+      this.head = null;
+      this.tail = null;
+      return value;
+    }
+
+    const removeLast = (node) => {
+      if (node.next === this.tail) {
+        const value = this.tail.value;
+        node.next = null;
+        this.tail = node;
+        return value;
+      }
+      return removeLast(node.next);
+    };
+    return removeLast(this.head);
+  };
+
+  // this.contains = (value) => {
+  //   const find = (node) => {
+  //     if (node === null) {
+  //       return false;
+  //     }
+  //     if (node.value === value) {
+  //       return true;
+  //     }
+  //     return find(node.next);
+  //   };
+  //   return find(this.head);
+  // };
+
+  this.contains = (value) => {
+    let temp = this.head;
+    while (temp !== null) {
+      if (temp.value === value) return true;
+      temp = temp.next;
+    }
+    return false;
+  };
+
+  this.find = (value) => {
+    let count = 0;
+    let temp = this.head;
+    while (temp !== null) {
+      if (temp.value === value) return count;
+      count++;
+      temp = temp.next;
+    }
+    return count;
+  };
+
+  this.toString = () => {
+    let str = "";
+    let temp = this.head;
+    while (temp !== null) {
+      str += `(${temp.value}) -> `;
+      temp = temp.next;
+    }
+    return str + "null";
+  };
+
+  this.insertAt = (value, index) => {};
 }
 
-function Node(value = null, nextNode = null) {
+function Node(value = null, next = null) {
   this.value = value;
+  this.next = next;
 }
 
 const list = new LinkedList();
 
 list.append("dog");
 list.append("cat");
-list.append("parrot");
-list.append("hamster");
-list.append("snake");
-list.append("turtle");
-list.toString();
+// list.append("parrot");
+// list.append("hamster");
+// list.append("snake");
+// list.append("turtle");
+list.prepend("anemal");
+console.log(list.toString());
+// console.log(list.last());
+// console.log(list.find("cat"));
