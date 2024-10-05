e62ba34d3ca1a44d7f41806ecbf05a786a073f7a
Brobo1 <armanahz95@gmail.com>
2024-10-05 17:38:36 +0200

    Add basic structure for Binary Search Tree
    - Create Node constructor
    - Implement initial Tree constructor
    - Include prettyPrint function for tree visualization
    - Set up placeholder for buildTree function
    - Test with sample array

---
+++ full-stack-js/javascript/projects/data-structures/binary-search-tree.js
@@ -0,0 +1,32 @@
+let testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+function Node(data) {
+  return {
+    data: data,
+    left: null,
+    right: null,
+  };
+}
+
+function Tree(arr) {
+  return { root: buildTree(arr) };
+}
+
+function buildTree(arr) {}
+
+buildTree();
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+prettyPrint(Tree(testArr));
