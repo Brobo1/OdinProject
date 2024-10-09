cb55bad1f17fa9f05b11d4f1dd37fe5824e138f2
Brobo1 <armanahz95@gmail.com>
2024-10-09 19:59:17 +0200

    Refactor buildBoard function to generate and print chessboard coordinates
    - Initialize and populate the board with coordinate pairs
    - Add debugging outputs for visualization of the board structure

--- full-stack-js/javascript/projects/data-structures/knights-travails/script.js
+++ full-stack-js/javascript/projects/data-structures/knights-travails/script.js
@@ -1,9 +1,19 @@
 function knightMoves() {}
 
 function buildBoard() {
-  for (let i = 7; i > 0; i--) {
-    for (let j = 0; j < 7; j++) {}
+  let board = [];
+  let str = "";
+  for (let x = 0; x <= 7; x++) {
+    let row = [];
+    for (let y = 0; y <= 7; y++) {
+      row.push([x, y]);
+      str += `(${x},${y}) `;
+    }
+    board.push(row);
+    str += "\n";
   }
+  console.log(str);
+  console.log(board[3][2]);
 }
 
 buildBoard();
