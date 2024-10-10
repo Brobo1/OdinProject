d70e7f289a64de54cebb0206124b785456519331
Brobo1 <armanahz95@gmail.com>
2024-10-10 14:55:10 +0200

    Refactor `knightMoves` function into `KnightMoves` class
    - Implement solve method to determine knight’s path.
    - Add getMoves method to fetch possible moves.
    - Add isValid method to validate moves.
    - Instantiate KnightMoves and execute solve method in script.

--- full-stack-js/javascript/projects/data-structures/knights-travails/script.js
+++ full-stack-js/javascript/projects/data-structures/knights-travails/script.js
@@ -1,4 +1,52 @@
-function knightMoves() {}
+function KnightMoves(x, y) {
+  this.row = x;
+  this.col = y;
+
+  this.solve = (path) => {
+    if (path.length === 64) return true;
+
+    const moves = path[path.length - 1].getMoves(path);
+
+    for (let i = 0; i < moves.length; i++) {
+      const move = moves[i];
+      path.push(move);
+    }
+
+    if (this.solve(path)) {
+      return true;
+    } else path.pop();
+    return false;
+  };
+
+  this.getMoves = (path) => {
+    if (!path) throw new Error("path was null");
+
+    const r = this.row;
+    const c = this.col;
+
+    return [
+      [r + 1, c + 2],
+      [r + 1, c - 2],
+      [r - 1, c + 2],
+      [r - 1, c - 2],
+      [r + 2, c + 1],
+      [r + 2, c - 1],
+      [r - 2, c + 1],
+      [r - 2, c - 1],
+    ].filter((cord) => cord.this.isValid(path));
+  };
+
+  this.isValid = (path) => {
+    if (!path) throw new Error("path was null");
+
+    for (let i = 0; i < path.length; i++) {
+      const sqr = path[i];
+      if (sqr.row === this.row && sqr.col === this.col) return false;
+    }
+
+    return this.row >= 0 && this.row <= 64 && this.col >= 0 && this.col <= 64;
+  };
+}
 
 function buildBoard() {
   let board = [];
@@ -15,5 +63,8 @@ function buildBoard() {
   console.log(str);
   console.log(board[3][2]);
 }
+
+const knight = new KnightMoves(0, 0);
+knight.solve([3, 4]);
 
 buildBoard();
