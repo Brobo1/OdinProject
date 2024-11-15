5af6f0bb9984d38fea458320ea272e03116e827d
brobo1 <armanahz95@gmail.com>
2024-11-15 14:54:19 +0100

    Add routers for authors, books, and index
    Replaced individual route handlers with router middleware for better modularity and organization. Commented out the old message routes for potential future use.

--- full-stack-js/node-js/exercises/express-exercises/routes-exercises/app.js
+++ full-stack-js/node-js/exercises/express-exercises/routes-exercises/app.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const app = express();
+const authorRouter = require("routes/authorRouter");
+const bookRouter = require("routes/bookRouter");
+const indexRouter = require("routes/indexRouter");
 
-app.get("/:username/messages", (req, res) => {
-  console.log(req.params);
-  res.end();
-});
+// app.get("/:username/messages", (req, res) => {
+//   console.log(req.params);
+//   res.end();
+// });
+//
+// app.get("/:username/messages/:messages", (req, res) => {
+//   console.log(req.params);
+//   console.log(req.query);
+//   res.end();
+// });
 
-app.get("/:username/messages/:messages", (req, res) => {
-  console.log(req.params);
-  console.log(req.query);
-  res.end();
-});
+app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
+app.use("/", indexRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
