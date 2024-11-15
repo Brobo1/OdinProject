79aaea1f62de1951b7d484fd68cc54a53fb503f8
Brobo1 <armanahz95@gmail.com>
2024-11-15 19:50:53 +0100

    Add routing structure with controllers and database interaction
    - Created `db.js` for author data and fetching logic
    - Introduced `authorController.js` to handle author requests
    - Configured `app.js` with routers for authors, books, and index
    - Added `authorRouter.js`, `bookRouter.js`, and `indexRouter.js` for respective routes
    - Renamed `routesrun.http` for consistency

--- full-stack-js/node-js/exercises/express-exercises/express-controllers/app.js
+++ full-stack-js/node-js/exercises/express-exercises/express-controllers/app.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
+const authorRouter = require("./routes/authorRouter");
+const bookRouter = require("./routes/bookRouter");
+const indexRouter = require("./routes/indexRouter");
 
-app.use((req, res) => {
-  res.send("Hello");
-  console.log("will still run!");
-  res.send("bye");
+// app.get("/:username/messages", (req, res) => {
+//   console.log(req.params);
+//   res.end();
+// });
+//
+// app.get("/:username/messages/:messages", (req, res) => {
+//   console.log(req.params);
+//   console.log(req.query);
+//   res.end();
+// });
+
+app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
+app.use("/", indexRouter);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
---
+++ full-stack-js/node-js/exercises/express-exercises/express-controllers/controllers/authorController.js
@@ -0,0 +1,13 @@
+const db = require("../db");
+
+async function getAuthorsById(req, res) {
+  const { authorId } = req.params;
+  const author = await db.getAuthorById(Number(authorId));
+  if (!author) {
+    res.status(404).send("Author not found");
+    return;
+  }
+  res.send(`Author Name: ${author.name}`);
+}
+
+module.exports = { getAuthorsById };
