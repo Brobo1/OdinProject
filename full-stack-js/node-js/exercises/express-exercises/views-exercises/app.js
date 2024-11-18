82a1f5410e9ce5a6f6970727bca4b5f607324b3b
brobo1 <armanahz95@gmail.com>
2024-11-18 15:36:55 +0100

    Enhance view rendering with dynamic data
    Added a new `user.ejs` partial to render individual users and updated `index.ejs` to include a navigation bar and iterate over a users list. Modified `app.js` to pass `links` and `users` data to the `index.ejs` template, replacing the static message and variable placeholders.

--- full-stack-js/node-js/exercises/express-exercises/views-exercises/app.js
+++ full-stack-js/node-js/exercises/express-exercises/views-exercises/app.js
@@ -5,8 +5,14 @@ const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+const users = ["Rose", "Cake", "Beef"];
+
 app.get("/", (req, res) => {
-  res.render("index", { message: "EJS rocks!", foo: "ppojepwojrgwrpgj" });
+  res.render("index", { links: links, users: users });
 });
 
 const PORT = 3000;
