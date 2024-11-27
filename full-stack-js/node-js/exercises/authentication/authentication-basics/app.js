6caae2a5586b66b007d213ef89a86f7c19d4079b
Brobo1 <armanahz95@gmail.com>
2024-11-27 20:50:22 +0100

    Implement basic user authentication
    - Add Passport.js for handling user authentication
    - Implement local strategy for username/password validation
    - Serialize and deserialize user for session management
    - Update routes for user login, logout, and signup
    - Modify index view to display login form or welcome message based on user authentication status

--- full-stack-js/node-js/exercises/authentication/authentication-basics/app.js
+++ full-stack-js/node-js/exercises/authentication/authentication-basics/app.js
@@ -27,9 +27,9 @@ app.use(
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
-app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+app.get("/", (req, res) => res.render("index", { user: req.user }));
 
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 app.post("/sign-up", async (req, res, next) => {
   try {
     await pool.query("insert into users (username, password) values ($1, $2)", [
@@ -40,6 +40,62 @@ app.post("/sign-up", async (req, res, next) => {
   } catch (err) {
     return next(err);
   }
+});
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query(
+        "SELECT * FROM users WHERE username = $1",
+        [username],
+      );
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }),
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
+      id,
+    ]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  }),
+);
+
+app.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
 });
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
